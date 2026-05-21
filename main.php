64db0400c7ac4b201db9c87ada7209ee006d2b43
green <jghouali@gmail.com>
2026-05-21 11:00:36 +0400

    Added user input validation for the list command

--- main.php
+++ main.php
@@ -2,4 +2,19 @@
 while (true) {
     $line = readline("Entrez votre commande : ");
     echo "Vous avez saisi : $line\n";
+    switch ($line) {
+        case 'list':
+            echo "affichage de la liste";
+            break;
+        default:
+            echo "help : affiche cette aide
+
+list : liste les contacts
+
+create [name], [email], [phone number] : crée un contact
+
+delete [id] : supprime un contact
+
+quit : quitte le programme";
+    }
 }
