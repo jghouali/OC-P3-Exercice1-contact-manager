cc2c9456deb42ec9b17979a19c79f5001948bd67
green <jghouali@gmail.com>
2026-05-22 01:16:23 +0400

    Added Command.php serving logics

--- main.php
+++ main.php
@@ -5,30 +5,10 @@ spl_autoload_register(static function ($fqcn): void {
     require_once $path;
 });
 
+use App\Controllers\Command;
 use App\Models\ContactManager;
 
 $contactManager = new ContactManager();
+$commandController = new Command();
 
-while (true) {
-    $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
-    switch ($line) {
-        case 'list':
-            echo "affichage de la liste" . PHP_EOL;
-            foreach ($contactManager->findAll() as $contact) {
-                echo $contact . PHP_EOL;
-            }
-            //var_dump($contactManager->findAll());
-            break;
-        default:
-            echo "help : affiche cette aide
-
-list : liste les contacts
-
-create [name], [email], [phone number] : crée un contact
-
-delete [id] : supprime un contact
-
-quit : quitte le programme";
-    }
-}
+$commandController->loopAndDispatch();
---
+++ src/Controllers/Command.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controllers;
+
+class Command
+{
+    public function loopAndDispatch(): void
+    {
+        while (true) {
+            $line = readline('> ');
+
+            $line = trim($line);
+
+            if ($line === '') {
+                continue;
+            }
+
+            $this->process($line);
+        }
+    }
+
+    public function process(string $command)
+    {
+        switch ($command) {
+            case 'help':
+                echo "Commande saisie : help\n";
+                break;
+            case 'list':
+                $this->list();
+                break;
+            case 'create':
+                echo "Commande saisie : create\n";
+                break;
+            case 'delete':
+                echo "Commande saisie : delete\n";
+                break;
+            case 'quit':
+                echo "Commande saisie : quit\n";
+                break;
+            default:
+                echo "Commande invalide : help\n";
+                break;
+        }
+    }
+
+    public function list(): void
+    {
+        echo "Commande saisie : list\n";
+    }
+}
